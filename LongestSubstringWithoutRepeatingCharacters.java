cf3464e0b6595b9c9668b8c2501cb66518649141
Rohit Singhal <rohit.singhal728@gmail.com>
2020-03-29 15:09:09 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java
    Refactoring

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -25,16 +25,16 @@ Explanation: The answer is "wke", with the length of 3.
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int l = s.length();
-        int ans = 0;
-        HashMap<Character, Integer> h = new HashMap<Character, Integer>();
+        Map<Character, Integer> h = new HashMap<>();    // to store character and its latest index
         int j = 0;
+        int ans = 0;                                    // to store maximum length
         for (int i = 0; i < l; i++) {
             char c = s.charAt(i);
-            if (h.containsKey(c)) {
-                j = Math.max (h.get(c), j);
+            if (h.containsKey(c)) {             // if character repeated
+                j = Math.max (h.get(c), j);     // move to previous charcter index + 1
             }
-            ans = Math.max (ans, i - j + 1);
-            h.put (c, i + 1);
+            ans = Math.max (ans, i - j + 1);    // maximum length found so far
+            h.put (c, i + 1);                   // (i+1); if repetition found then will move to previous repetition + 1
         }
         return ans;
     }
