64209e8069437fc6ad42405311c5ad6d9a9ffdb5
Rohit Singhal <rohit.singhal728@gmail.com>
2020-06-09 13:06:44 +0530

    Create 392. IsSubsequence.java

---
+++ 392. IsSubsequence.java
@@ -0,0 +1,72 @@
+/*
+
+Link : https://leetcode.com/problems/is-subsequence/
+
+Given a string s and a string t, check if s is subsequence of t.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+0 <= s.length <= 100
+0 <= t.length <= 10^4
+Both strings consists only of lowercase characters.
+
+*/
+
+
+// Solution1 : Iterative
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int slen = s.length();
+        int tlen = t.length();
+        
+        int j = 0;
+        for (int i = 0; i < tlen && j < slen; i++) {
+            if (s.charAt(j) == t.charAt(i))
+                j ++;
+        }
+        
+        return j == slen;
+    }
+}
+
+
+
+// Solution2 : Recursive
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int slen = s.length();
+        int tlen = t.length();
+        
+        return isSubs(s, slen, t, tlen);
+    }
+    
+    public boolean isSubs(String s, int i, String t, int j) {
+        
+        if (i == 0)
+            return true;
+        if (j == 0)
+            return false;
+        
+        if (s.charAt(i - 1) == t.charAt(j - 1))
+            return isSubs(s, i - 1, t, j - 1);
+        
+        return isSubs(s, i, t, j - 1);
+    }
+}
