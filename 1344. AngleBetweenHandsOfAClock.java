a227d35ad776f55fa1350c16f77fa244d57e3796
Rohit Singhal <rohit.singhal728@gmail.com>
2020-07-14 19:09:08 +0530

    Create 1344. AngleBetweenHandsOfAClock.java

---
+++ 1344. AngleBetweenHandsOfAClock.java
@@ -0,0 +1,50 @@
+/*
+
+Link : https://leetcode.com/problems/angle-between-hands-of-a-clock/
+
+Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
+
+Example 1:
+Input: hour = 12, minutes = 30
+Output: 165
+
+Example 2:
+Input: hour = 3, minutes = 30
+Output: 75
+
+Example 3:
+Input: hour = 3, minutes = 15
+Output: 7.5
+
+Example 4:
+Input: hour = 4, minutes = 50
+Output: 155
+
+Example 5:
+Input: hour = 12, minutes = 0
+Output: 0
+
+Constraints:
+1 <= hour <= 12
+0 <= minutes <= 59
+
+Answers within 10^-5 of the actual value will be accepted as correct.
+
+*/
+
+
+
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        
+        int a = minutes * 6;
+        double b = minutes * 0.5;
+        
+        if (hour != 12)
+            b += hour * 30;
+        
+        double angle = Math.abs(a - b);
+        
+        return angle < 180 ? angle : 360 - angle;
+    }
+}
