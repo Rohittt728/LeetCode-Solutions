e158a93021a4cdfa9c807de7523d15b99946e4b1
Rohit Singhal <rohit.singhal728@gmail.com>
2020-07-21 10:38:44 +0530

    Update 203. RemoveLinkedListElements.java

--- 203. RemoveLinkedListElements.java
+++ 203. RemoveLinkedListElements.java
@@ -41,3 +41,22 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+
+
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+       if (head == null)
+           return null;
+        
+        head.next = removeElements(head.next, val);
+        
+        if (head.val == val)
+            return head.next;
+        
+        return head;
+    }
+}
