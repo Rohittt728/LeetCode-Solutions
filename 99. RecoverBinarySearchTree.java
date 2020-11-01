5ba1e051ab5bb3bf41a79d959a2b24a9777ea075
Rohit Singhal <rohit.singhal728@gmail.com>
2020-11-01 16:48:54 +0530

    Create 99. RecoverBinarySearchTree.java

---
+++ 99. RecoverBinarySearchTree.java
@@ -0,0 +1,74 @@
+/*
+
+Link : https://leetcode.com/problems/recover-binary-search-tree/
+
+You are given the root of a binary search tree (BST), where exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+Follow up: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+Example 1:
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+Example 2:
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+
+Constraints:
+The number of nodes in the tree is in the range [2, 1000].
+-231 <= Node.val <= 231 - 1
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+// Solution 1 : Inorder Traversal (Recursive)
+class Solution {
+    
+    TreeNode first, second, prev;
+    
+    public void recoverTree(TreeNode root) {
+        
+        inorderTraversal(root);
+        
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    public void inorderTraversal(TreeNode root) {
+        if (root == null)
+            return;
+        
+        inorderTraversal(root.left);
+        
+        if (first == null && (prev == null || prev.val >= root.val))
+            first = prev;
+        
+        if (first != null && prev.val >= root.val)
+            second = root;
+        
+        prev = root;
+        
+        inorderTraversal(root.right);
+    }
+}
