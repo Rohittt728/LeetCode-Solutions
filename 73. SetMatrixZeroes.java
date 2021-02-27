eeb236c131eb463563e888d2fc75d98ef55b4d5f
Rohit Singhal <rohit.singhal728@gmail.com>
2021-02-27 09:34:59 +0530

    Update 73. SetMatrixZeroes.java

--- 73. SetMatrixZeroes.java
+++ 73. SetMatrixZeroes.java
@@ -40,7 +40,7 @@ Follow up:
 */
 
 
-
+// Solution 1
 class Solution {
     public void setZeroes(int[][] matrix) {
         
@@ -96,6 +96,37 @@ class Solution {
         // handling first column
         if (fCol == 0) {
             for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
+}
+
+
+// Solution 2 : Better approach
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int fcol = 1;
+        
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0)
+                fcol = 0;
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if (fcol == 0)
                 matrix[i][0] = 0;
         }
     }
