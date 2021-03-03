3413865dbc3b0b59760d229a2ca90895d9637fc0
Rohit Singhal <rohit.singhal728@gmail.com>
2021-03-03 17:19:29 +0530

    Update 62. UniquePaths.java

--- 62. UniquePaths.java
+++ 62. UniquePaths.java
@@ -65,3 +65,20 @@ class Solution {
         return dp[n - 1];
     }
 }
+
+
+// Solution 3 : Most Optimal
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+        int p = m + n - 2;
+        int r = m - 1;
+        double ans = 1;
+        
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (p - r + i) / i;
+        }
+        
+        return (int)ans;
+    }
+}
