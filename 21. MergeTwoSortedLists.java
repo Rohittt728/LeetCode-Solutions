98429f5dbace2ef09a2ec5c1ca1aa91a89296fd3
Rohit Singhal <rohit.singhal728@gmail.com>
2021-03-04 23:42:03 +0530

    Update 21. MergeTwoSortedLists.java

--- 21. MergeTwoSortedLists.java
+++ 21. MergeTwoSortedLists.java
@@ -93,3 +93,36 @@ class Solution {
         }
     }
 }
+
+// Solution 3 : Iterative (best)
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        
+        if (l1.val > l2.val) {
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+        
+        ListNode res = l1;
+        while (l1 != null && l2 != null) {
+            
+            ListNode prev1 = null;
+            while (l1 != null && l1.val <= l2.val) {
+                prev1 = l1;
+                l1 = l1.next;
+            }
+            prev1.next = l2;
+            
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+        return res;
+    }
+}
